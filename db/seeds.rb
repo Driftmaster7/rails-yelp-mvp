caacf92e39fbe92d446b9ffc34e4744c884198ca
Yann Bouhadana <yannbouhadana@gmail.com>
2022-05-11 14:32:10 -0400

    making progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+restaurant_one = Restaurant.create(name: "Mcdonalds", address: "elmo street", phone_number: "01234 578990", category: "chinese")
+restaurant_two = Restaurant.create(name: "tim hortons", address: "wagon street", phone_number: "01234 678990", category: "italian")
+restaurant_three = Restaurant.create(name: "burger king", address: "st-clare ave", phone_number: "10234 578990", category: "french")
+restaurant_four = Restaurant.create(name: "slice n soda", address: "idk street", phone_number: "01243 578990", category: "japanese")
+restaurant_five = Restaurant.create(name: "pizza hut", address: "hello street", phone_number: "01324 578990", category: "belgian")
